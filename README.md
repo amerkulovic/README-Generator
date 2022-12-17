50a5f7b71ea5a157e66676a2e258cecaffc63bee
Amer Kulovic <amer.kulovic@yahoo.com>
2022-12-16 21:59:10 -0600

    added more questions

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # MyMovieSpace
 ## Description
-  To find the movies you love
+  A place to find and discover your favorite movies
+## Table of Contents
+## Installation
+  We deployed it through GitHub
+## Usage
+  To help people find movies!
+## License
+ N/A
+## Contributions
+  A trailer for the single movie page
+## Tests
+  A whole lot
+## Questions
+  No, the project is perfect
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 const questions = [
   {
     type: "input",
@@ -14,14 +15,60 @@ const questions = [
     message: "What is the description of your project?",
     name: "description",
   },
+  {
+    type: "input",
+    message: "What was the installation process of your project?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "What is the usage of your project?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "What is the license you used?",
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "What are the contribtions you would like to add to this project?",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "What testing have you done?",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: "If you have any questions please feel free to ask?",
+    name: "questions",
+  },
 ];
+
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
   const exportData = generateMarkdown(data);
   const template = `${exportData}
 ## Description
-  ${data.description}`
+  ${data.description}
+## Table of Contents
+## Installation
+  ${data.installation}
+## Usage
+  ${data.usage}
+## License
+ ${data.license}
+## Contributions
+  ${data.contribution}
+## Tests
+  ${data.test}
+## Questions
+  ${data.questions}`;
+
   return fs.writeFileSync(fileName, template);
 }
 
